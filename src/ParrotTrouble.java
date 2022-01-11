ea2d83171f07b54c90f50f76bc82f4723d8240c7
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-11 09:01:36 +0530

    Solution to codingbat ParrotTrouble- Question

---
+++ src/ParrotTrouble.java
@@ -0,0 +1,10 @@
+public class ParrotTrouble {
+    public static void main(String[] args) {
+        System.out.println(parrotTrouble(true,10));
+
+    }
+    public static boolean parrotTrouble(boolean talking, int hour) {
+
+        return (talking && (hour < 7 || hour > 20));
+    }
+}
