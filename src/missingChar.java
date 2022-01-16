b9b999a53eca87b2e515a5d77dddabedea81db76
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-16 15:58:30 +0530

    Solution to codingbat SumDouble- Question

--- src/missingChar.java
+++ src/missingChar.java
@@ -1,5 +1,8 @@
 public class missingChar {
-        public String missingChar(String str, int n) {
+    public static void main(String[] args) {
+        System.out.println(missingChar("hellows",7));
+    }
+        public static String missingChar(String str, int n) {
             String front = str.substring(0, n);
             String back = str.substring(n +1 , str.length());
             return front + back;
