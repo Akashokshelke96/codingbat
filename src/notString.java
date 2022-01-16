6e7ce022eb48a4e29719251f84ccc5ac49aebac6
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-16 15:52:51 +0530

    Solution to codingbat SumDouble- Question

--- src/notString.java
+++ src/notString.java
@@ -1,4 +1,8 @@
 public class notString {
+    public void main(String[] args) {
+        System.out.println(notString("hello"));
+    }
+
     public String notString(String str) {
         if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
             return str;
