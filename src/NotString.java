02b6a62add93d29c414b7cf1cf2d65aea39259c1
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-16 15:59:30 +0530

    Solution to codingbat Notstring - Question

---
+++ src/NotString.java
@@ -0,0 +1,24 @@
+public class NotString {
+    public static void main(String[] args) {
+        System.out.println(notString("hello"));
+    }
+
+    public static String notString(String str) {
+        if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+            return str;
+        } else {
+            return "not " + str;
+        }
+    }
+}
+
+//    public static void main(String[] args) {
+//        System.out.println(parrotTrouble(true, 10));
+//
+//    }
+//
+//    public static boolean parrotTrouble(boolean talking, int hour) {
+//
+//        return (talking && (hour < 7 || hour > 20));
+//    }
+//}
