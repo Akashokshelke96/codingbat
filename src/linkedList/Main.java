941747b4690e152a617689838e940a1f5d984951
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-19 12:52:40 +0530

    learning Linked List

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -1,5 +1,7 @@
 package linkedList;
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         Node ten = new Node(10);
@@ -13,19 +15,35 @@ public class Main {
         thirty.setNext((fourty));
         fourty.setNext((fifty));
         fifty.setNext((null));
+        insertAtFront(head,5);
         printLinkedlist(head);
     }
 
     public static void printLinkedlist(Node head) {
 
         Node curr = head;
-        while (curr != null) {
+
+        while(curr.getNext() != null) {
             System.out.println(curr.getData());
             curr = curr.getNext();
 
 
 
 
+
         }
+    }
+    public static void insertAtFront(Node head, int data){
+        Node nn = new Node(data);
+        nn.setNext(null);
+
+        if (head == null) {
+            head = nn;
+        }
+        else {
+            nn.setNext((head));
+            head=nn;
+        }
+        return;
     }
 }
