52689813faaa44f3c1ce6589ae06c9f54094851f
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-19 09:39:26 +0530

    learning Linked List

---
+++ src/linkedList/Main.java
@@ -0,0 +1,19 @@
+package linkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Node ten = new Node(10);
+        Node twenty = new Node(20);
+        Node thirty = new Node(30);
+        Node fourty = new Node(40);
+        Node fifty = new Node(50);
+        Node head = ten;
+        ten.setNext(twenty);
+        twenty.setNext(thirty);
+        thirty.setNext((fourty));
+        fourty.setNext((fifty));
+        fifty.setNext((null));
+        System.out.println(fourty.getNext());
+
+    }
+}
