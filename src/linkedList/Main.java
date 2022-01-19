b42584c03ad15a82866f277f3c276306d919bb3a
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-19 13:42:32 +0530

    learning Linked List

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -15,35 +15,46 @@ public class Main {
         thirty.setNext((fourty));
         fourty.setNext((fifty));
         fifty.setNext((null));
-        insertAtFront(head,5); // method to insert items at front of the linked list;
+
+        insertAtFront(head, 5); // method to insert items at front of the linked list;
+
         printLinkedlist(head);//method to print the linked list
+
+        head = deleteAtFront(head);
+        printLinkedlist(head);
+
+        head = deleteAtFront(head);
+        printLinkedlist(head);
     }
 
     public static void printLinkedlist(Node head) {
-
+        System.out.println("Printing the Linked List!!");
         Node curr = head;
-
-        while(curr.getNext() != null) {
-            System.out.println(curr.getData());
+        while (curr.getNext() != null) {                                                //here using the .getnext we stopped the iteration just before the last element
+            System.out.println(curr.getData());              //adding another next .getNext we can similarly access 3rd last point
             curr = curr.getNext();
-
-
-
 
 
         }
     }
-    public static void insertAtFront(Node head, int data){
+
+    public static void insertAtFront(Node head, int data) {
         Node nn = new Node(data);
         nn.setNext(null);
 
         if (head == null) {
             head = nn;
-        }
-        else {
+        } else {
             nn.setNext((head));
-            head=nn;
+            head = nn;
         }
         return;
+    }
+
+    public static Node deleteAtFront(Node head) {
+        if (head != null) {
+            head = head.getNext();
+        }
+        return head;
     }
 }
