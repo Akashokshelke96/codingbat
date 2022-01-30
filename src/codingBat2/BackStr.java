fba0672587501469f854a576ff0df06cbed94b73
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-30 23:19:39 +0530

    learning codingbat questions strings

---
+++ src/codingBat2/BackStr.java
@@ -0,0 +1,16 @@
+package codingBat2;
+
+public class BackStr {
+    public static String frontBack(String str) {
+        if (str.length() <= 1) return str;
+
+        String mid = str.substring(1, str.length()-1);
+
+        // last + mid + first
+        return str.charAt(str.length()-1) + mid + str.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(frontBack("akash"));
+    }
+}
