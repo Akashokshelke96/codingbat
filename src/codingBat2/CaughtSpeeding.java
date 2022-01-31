e8bf3f833bc8c5e87fa0802cf0d3292307a056a1
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-31 23:44:30 +0530

    learning codingbat questions CaughtSpeeding

---
+++ src/codingBat2/CaughtSpeeding.java
@@ -0,0 +1,33 @@
+package codingBat2;
+
+public class CaughtSpeeding {
+    public static int caughtSpeeding(int speed, boolean isBirthday) {
+        int lolimit = 60;
+        int highlimit = 80;
+
+        if( isBirthday == true) {
+            lolimit = lolimit + 5;
+            highlimit = highlimit + 5;
+            if( speed > lolimit && speed <= highlimit) {
+                return 1;
+            }
+            else if (speed > highlimit){
+                return 2;
+            }
+        }
+        else if (isBirthday ==  false){
+            if( speed > lolimit && speed <= highlimit) {
+                return 1;
+            }
+            else if (speed >= highlimit){
+                return 2;
+            }
+
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(caughtSpeeding(80,true));
+    }
+}
