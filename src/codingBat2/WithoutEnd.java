cab16ddb0f2fb4fc472fc58e34b50d9597511f10
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-31 23:36:25 +0530

    learning codingbat questions strings

--- src/codingBat2/WithoutEnd.java
+++ src/codingBat2/WithoutEnd.java
@@ -1,17 +1,16 @@
 package codingBat2;
 
 public class WithoutEnd {
-    public static String withoutEnd2(String str) {
-        if (str.length() > 2)
-        {
-            return str.substring(1,str.length() - 1);
-        }
-        else {
-            return "";
-        }
-    }
-
     public static void main(String[] args) {
-        System.out.println(withoutEnd2("hello"));
+        System.out.println(withoutEnd2(2));
+    }
+    public static int[] withoutEnd2(Integer integer) {
+        int[] nums = new int[0];
+        int larger = Math.max(nums[0], nums[2]);
+        nums[0] = larger;
+        nums[1] = larger;
+        nums[2] = larger;
+        System.out.println(nums);
+        return nums;
     }
 }
