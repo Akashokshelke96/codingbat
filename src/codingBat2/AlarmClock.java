7555136f878e6e39b8719e52ba6a40d49a9c6ef8
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-01 23:09:10 +0530

    Solving codingbat questions AlarmClock.

---
+++ src/codingBat2/AlarmClock.java
@@ -0,0 +1,24 @@
+package codingBat2;
+
+public class AlarmClock {
+    public static void main(String[] args) {
+        System.out.println(alarmClock(2,true));
+    }
+    public static String alarmClock(int day, boolean vacation) {
+        if((day >= 1  && day <=5) && vacation == false){
+            return "7:00";
+        }
+        else if ((day == 0  || day > 5)  && vacation == true){
+            return "off";
+
+        }
+        else if ((day >= 1  && day <=5) && vacation == true){
+            return "10:00";
+
+        }
+        else if ((day == 0  || day > 5)  && vacation == false){
+            return "10:00";
+        }
+        return "7:00";
+    }
+}
