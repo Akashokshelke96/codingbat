61bfb2562bdc92ef9d5f02b8f1d60a905d3426f4
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-01 23:09:31 +0530

    Solving codingbat questions AnswerCell.

---
+++ src/codingBat2/AnswerCell.java
@@ -0,0 +1,27 @@
+package codingBat2;
+
+public class AnswerCell {
+    public static void main(String[] args) {
+        System.out.println(answerCell(true,true,false));
+    }
+    public static boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+        if( ((isMorning == false)||(isMorning == true)) && ((isMom == false)||( isMom == true)) && isAsleep == true) {
+            return false;
+
+        }
+        else if (isMorning == true && isMom == false && isAsleep == false){
+            return false;
+        }
+        else if(isMorning == false && isMom == true && isAsleep == false){
+            return true;
+
+        }
+        else if (isMorning == false && isMom == false && isAsleep == false){
+            return true;
+        }
+        else {
+            return true;
+        }
+    }
+
+}
