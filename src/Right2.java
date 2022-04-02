5bc428b81cd644cbaff69d8236313b616ea9eabf
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-02 22:04:19 +0530

    Solving codingbat questions

---
+++ src/Right2.java
@@ -0,0 +1,12 @@
+public class Right2 {
+    public static void main(String[] args) {
+        System.out.println(right2("yallo"));
+    }
+    public  static String right2(String str) {
+        if(str.length() > 2){
+            return (str.substring(str.length() - 2 , str.length()) + str.substring(0,str.length() - 2));
+        }else{
+            return str;
+        }
+    }
+}
