e1f1fbc4011234f8abb137f1b8878a42e7220963
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-02 22:01:38 +0530

    Solving codingbat questions

--- src/Logic2/Old35.java
+++ src/Logic2/Old35.java
@@ -3,9 +3,12 @@ package Logic2;
 public class Old35 {
     //Return true if the given non-negative number is a multiple of 3 or 5, but not both. Use the % "mod" operator
     public static void main(String[] args) {
-        System.out.println(old35(15));
-    }
-    public static boolean old35(int n) {
-        return((n % 3 == 0 && n % 5 != 0) || (n % 3 != 0 && n % 5 == 0));
+       byte x = 10;
+       byte y = 20;
+       x++;
+       ++y;
+       y++;
+        System.out.println(x + " ");
+        System.out.println();
     }
 }
