534402caf6e2b5625f6bc9f895d7af74dac27a8d
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-07 23:31:25 +0530

    Solving codingbat questions

---
+++ src/maxMod5.java
@@ -0,0 +1,15 @@
+public class maxMod5 {
+
+    public static void main(String[] args) {
+        System.out.println(maxMod5(4,6));
+
+    }
+    public static int maxMod5(int a, int b) {
+        if(a == b)
+            return 0;
+        if(a % 5 == b % 5)
+            return (a < b) ? a : b;
+        return (a > b) ? a : b;
+    }
+
+}
