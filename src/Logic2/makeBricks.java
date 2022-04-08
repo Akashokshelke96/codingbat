3dab9f8c0ff6c6a03d5a61d0bddc11a361b5277c
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-08 22:52:41 +0530

    Solving codingbat questions

--- src/Logic2/makeBricks.java
+++ src/Logic2/makeBricks.java
@@ -3,6 +3,8 @@ package Logic2;
 public class makeBricks {
     public static void main(String[] args) {
         System.out.println(makeBricks(1,5,86));
+        //We want to make a row of bricks that is goal inches long. We have a number of small bricks
+        // (1 inch each) and big bricks (5 inches each). Return true if it is possible to make the goal by choosing from the given bricks.
     }
     public static boolean makeBricks(int small, int big, int goal) {
         if(goal > big*5 + small){
