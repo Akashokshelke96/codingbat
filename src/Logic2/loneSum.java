fbc698b7b9ffe8a01de4e932b4c8f40963f41105
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-08 22:52:12 +0530

    Solving codingbat questions

--- src/Logic2/loneSum.java
+++ src/Logic2/loneSum.java
@@ -3,6 +3,8 @@ package Logic2;
 public class loneSum {
     public static void main(String[] args) {
         System.out.println(loneSum(3,5,10));
+        //Given 3 int values, a b c, return their sum. However, if one of the values is the same as another
+        // of the values, it does not count towards the sum.
     }
     public static int loneSum(int a, int b, int c) {
         if((a == b) && (b == c)){
