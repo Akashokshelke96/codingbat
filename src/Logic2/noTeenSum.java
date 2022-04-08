e2a69bbf105e511adf374b696285b5ef01f1832d
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-08 22:52:55 +0530

    Solving codingbat questions

--- src/Logic2/noTeenSum.java
+++ src/Logic2/noTeenSum.java
@@ -3,6 +3,8 @@ package Logic2;
 public class noTeenSum {
     public static void main(String[] args) {
         System.out.println(noTeenSum(14,13,12));
+        //Given 3 int values, a b c, return their sum. However, if any of the values is a teen --
+        // in the range 13..19 inclusive -- then that value counts as 0, except 15 and 16 do not count as a teens.
     }
     public  static int noTeenSum(int a, int b, int c) {
 
