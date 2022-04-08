7b28f2d4b8530989bd6ac24759e3a38009de9912
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-08 22:52:31 +0530

    Solving codingbat questions

--- src/Logic2/luckySum.java
+++ src/Logic2/luckySum.java
@@ -3,6 +3,8 @@ package Logic2;
 public class luckySum {
     public static void main(String[] args) {
         System.out.println(luckySum(12,34,64));
+        //Given 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not
+        // count towards the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count.
     }
     public static int luckySum(int a, int b, int c) {
         if( a == 13){
