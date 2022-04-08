48378480f2456c8867f713ec907150c5f4f7b041
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-08 22:51:47 +0530

    Solving codingbat questions

--- src/Logic2/CloseFar.java
+++ src/Logic2/CloseFar.java
@@ -2,6 +2,8 @@ package Logic2;
 
 public class CloseFar {
     public static void main(String[] args) {
+        //Given three ints, a b c, return true if one of b or c is "close" (differing from a by at most 1),
+        // while the other is "far", differing from both other values by 2 or more.
         System.out.println(closeFar(23,45,78));
     }
     public static boolean closeFar(int a, int b, int c) {
