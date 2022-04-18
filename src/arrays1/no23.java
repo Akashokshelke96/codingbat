a1433f1ab49c208c71e996733323af7742a82135
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-18 21:50:54 +0530

    Solving codingbat questions

--- src/arrays1/no23.java
+++ src/arrays1/no23.java
@@ -3,7 +3,9 @@ package arrays1;
 public class no23 {
     public static void main(String[] args) {
         int[] nums = new int[]{1,2};
-        System.out.println(no23(nums));
+        for (int element: nums) {
+            System.out.println(element);
+        }
     }
     public static boolean no23(int[] nums) {
         return (((nums[0] != 2 && nums[1] != 2)) && ((nums[0] != 3 && nums[1] != 3)));
