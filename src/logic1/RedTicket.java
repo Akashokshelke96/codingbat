72683817f162cc72c0e3a0282b3814b3ccac8376
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-22 23:27:25 +0530

    Solving codingbat questions

---
+++ src/logic1/RedTicket.java
@@ -0,0 +1,21 @@
+package logic1;
+
+public class RedTicket {
+    public static void main(String[] args) {
+        System.out.println(redTicket(12,13,14));
+    }
+    public static int redTicket(int a, int b, int c) {
+        if((a == 2) &&  (b == 2) && (c == 2)){
+            return 10;
+        }
+        else if((a == b) && (b == c)){
+            return 5;
+        }
+        else if((b!=a)&& (c!=a)){
+            return 1;
+        }
+
+        return 0;
+    }
+
+}
