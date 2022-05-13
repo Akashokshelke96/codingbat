58a8d4e3e03a1fe8d3f3d8033d41f85d118e5bf4
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-05-13 23:16:52 +0530

    Solving codingbat question

---
+++ src/Strings/twoChar.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class twoChar {
+    public static void main(String[] args) {
+        //Given a string and an index, return a string length 2 starting at the given index.
+        // If the index is too big or too small to define a string length 2, use the first 2 chars.
+        // The string length will be at least 2.
+        System.out.println(twoChar("akashS",3));
+    }
+    public static String twoChar(String str, int index) {
+        if (str.length() <= index + 1 || index < 0){
+            return str.substring(0,2);
+        }
+        else{
+
+            return str.substring(index, index + 2);
+        }
+    }
+}
