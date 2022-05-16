7b4c1340ab4607244234c3ee457a94f9aa20c632
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-05-16 23:06:43 +0530

    Solving codingbat Question STRING doubleX

---
+++ src/Strings/doubleX.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class doubleX {
+    public static void main(String[] args) {
+        //Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+        System.out.println(doubleX("xxkashxxx"));
+    }
+    static boolean doubleX(String str) {
+        for(int i = 0; i < str.length() - 1; i++) {
+            if(str.charAt(i) == 'x') {
+                if(str.charAt(i + 1) == 'x')
+                    return true;
+                else
+                    return false;
+            }
+        }
+        return false;
+    }
+}
