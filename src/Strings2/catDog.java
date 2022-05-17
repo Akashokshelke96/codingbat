ff73c1fa196f917af2de59030457a69e0f8d7a3a
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-05-17 23:43:13 +0530

    Solving codingbat Question STRINGS-2 countHi.java

---
+++ src/Strings2/catDog.java
@@ -0,0 +1,23 @@
+package Strings2;
+
+public class catDog {
+    public static void main(String[] args) {
+        System.out.println(catDog("catcatdog"));
+        //Return true if the string "cat" and "dog" appear the same number of times in the given string.
+    }
+    public static boolean catDog(String str) {
+        int cat = 0;
+        int dog = 0;
+
+        for(int i = 0; i < str.length() - 2; i++) {
+            if(str.substring(i, i + 3).equals("cat"))
+                cat++;
+
+            if(str.substring(i, i + 3).equals("dog"))
+                dog++;
+        }
+
+        return cat == dog;
+    }
+
+}
