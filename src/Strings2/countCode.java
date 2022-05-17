ec4b9e1b7a83a2d053d939b1882e369ae978675c
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-05-18 00:47:51 +0530

    Solving codingbat Question STRINGS-2 endOther.java

---
+++ src/Strings2/countCode.java
@@ -0,0 +1,18 @@
+package Strings2;
+
+public class countCode {
+    public static void main(String[] args) {
+        System.out.println(countCode("coxecodecodecole"));
+    }
+    public static int countCode(String str) {
+        int count = 0;
+
+
+        for(int i=0;i<str.length()-3;i++){
+            if(str.substring(i,i+2).equals("co") && str.substring(i+3,i+4).equals("e")){
+                count++;
+            }
+        }return count;
+
+    }
+}
---
+++ src/Strings2/endAnother.java
@@ -0,0 +1,18 @@
+package Strings2;
+
+public class endAnother {
+    public static void main(String[] args) {
+        //Given two strings, return true if either of the strings appears at the very end of the other string,
+        // ignoring upper/lower case differences
+        System.out.println(endOther("akabc","niabc"));
+    }
+    public static boolean endOther(String a, String b) {
+        if(a.length() < b.length()) {
+            String temp = a;
+            a = b.toLowerCase();
+            b = temp.toLowerCase();
+        }
+
+        return a.substring(a.length() - b.length()).equals(b);
+    }
+}
