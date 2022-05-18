2cfac70423fb5a787f50da779bf7d1475d4b35e2
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-05-18 23:14:42 +0530

    Solving codingbat Question STRINGS-2 xyBalance.java

---
+++ src/Strings2/xyBalance.java
@@ -0,0 +1,25 @@
+package Strings2;
+
+public class xyBalance {
+    public static void main(String[] args) {
+        /*We'll say that a String is xy-balanced if for all the 'x' chars in the string,
+        there exists a 'y' char somewhere later in the string. So "xxy" is balanced, but "xyx" is not.
+         One 'y' can balance multiple 'x's. Return true if the given string is xy-balanced.
+         */
+        System.out.println(xyBalance("xyxxxy"));
+    }
+    public static boolean xyBalance(String str) {
+        boolean foundY = false;
+        for(int i = str.length()-1 ;i >= 0 ;i--){
+
+
+            if(str.charAt(i)=='y'){
+                foundY = true;
+            }
+            if(str.charAt(i) == 'x' && foundY==false){
+                return false;
+            }
+
+        }return true;
+    }
+}
