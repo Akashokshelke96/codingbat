fc69bbedc80de383593356c2472a4114bd463660
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-05-26 15:54:58 +0530

    Solving pattern 1-0Triangle

--- src/patterns/BinaryTriangle.java
+++ src/patterns/BinaryTriangle.java
@@ -10,10 +10,10 @@ public class BinaryTriangle {
             for(int j = 1;j<=i ; j++){  //when the coordinates of any index sums to EVEN it prints "1" ,else when Odd it Prints "0"
                 int sum = i+j;
                 if(sum%2==0){
-                    System.out.print("1");
+                    System.out.print("1 ");
                 }
                 else if(sum%2==1){
-                    System.out.print("0");
+                    System.out.print("0 ");
                 }
             }
             System.out.println();
