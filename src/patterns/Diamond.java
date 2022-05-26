2ddf11a0d364412abf43bd2986e5795f0178f663
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-05-26 23:47:03 +0530

    Solving pattern Diamond Pattern

---
+++ src/patterns/Diamond.java
@@ -0,0 +1,35 @@
+package patterns;
+
+public class Diamond {
+    public static void main(String[] args) {
+
+        int n =6;
+
+        for(int i = 1 ; i<=n;i++){
+            //printing the spaces..no of spaces is n-i
+            for(int j =1 ; j<= n-i ; j++){
+                System.out.print(" ");
+
+            }
+            //printing the stars..no of stars is 2*i-1
+
+            for(int j = 1; j<= 2*i - 1 ; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for(int i = n; i >= 1 ;i--){
+            for(int j =1 ; j<= n-i ; j++){
+                System.out.print(" ");
+
+            }
+            for(int j = 1; j<= 2*i - 1 ; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
