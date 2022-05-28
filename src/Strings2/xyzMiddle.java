bb948f0d095031b6788bf106658c32941361204f
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-05-28 23:17:16 +0530

    solution codingBat xyzMiddle

---
+++ src/Strings2/xyzMiddle.java
@@ -0,0 +1,21 @@
+package Strings2;
+
+public class xyzMiddle {
+    public static void main(String[] args) {
+        System.out.println(xyzMiddle("abcdxyzrtyp"));
+    }
+    public static boolean xyzMiddle(String str) {
+        if(str.length() < 3)
+            return false;
+
+        int start1 = str.length() / 2 - 2;
+        int start2 = str.length() / 2 - 1;
+
+        if(str.length() % 2 == 0) {
+            return str.substring(start1, start1 + 3).equals("xyz") ||
+                    str.substring(start2, start2 + 3).equals("xyz");
+        }
+
+        return str.substring(start2, start2 + 3).equals("xyz");
+    }
+}
