87c3b061a8efe191cd8d1924b124a6ae1a589128
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-06-05 12:17:34 +0530

    solution codingBat oneTwo

---
+++ src/Strings2/oneTwo.java
@@ -0,0 +1,17 @@
+package Strings2;
+
+public class oneTwo {
+    public static void main(String[] args) {
+//Given a string, compute a new string by moving the first char to come after the next two chars, so "abc" yields "bca".
+// Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd".
+// Ignore any group of fewer than 3 chars at the end
+        System.out.println(onetwo("tcagdo"));
+    }
+    public static String onetwo(String str){
+            String str2 = "";
+            for(int i = 0 ; i < str.length() -2 ; i+=3){
+                str2 = str2 + (str.substring(i+1,i+3) + str.charAt(i));
+            }
+            return str2;
+        }
+}
